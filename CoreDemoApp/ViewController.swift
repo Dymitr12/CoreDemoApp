cc96a8cb0f8db5b59451bae25a29260e872b288b
Dymitr12 <dk39fra@gmail.com>
2022-04-18 20:03:10 +0300

    Initial Commit

---
+++ CoreDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDemoApp
+//
+//  Created by Dymitr on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
